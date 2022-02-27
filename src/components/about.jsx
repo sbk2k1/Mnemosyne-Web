b63c747f96f8049591fd2a35c6b3c5a2266c990a
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2022-02-28 01:21:05 +0530

    Website

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ export const About = (props) => {
           </div>
           <div className='col-xs-12 col-md-6'>
             <div className='about-text'>
-              <h2 style={{color: "white"}}>About Us</h2>
+              <h2 style={{color: "white"}}>About Me</h2>
               <p>{props.data ? props.data.paragraph : 'loading...'}</p>
               <h3 style={{color: "white"}}>Tech Stacks Conversant In</h3>
               <div className='list-style'>
